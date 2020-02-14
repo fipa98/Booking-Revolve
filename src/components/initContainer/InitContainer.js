2fe67fc4f45a5aa32223de7452ab6cf320d18a9c
Filip Jacobsen <fijaco98@gmail.com>
2020-02-14 15:07:25 +0100

    create initial window component

---
+++ src/components/initContainer/InitContainer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./InitContainer.css";
+
+class InitContainer extends React.Component {
+    render() {
+        return (
+            <div className="initContainerRoot">
+                <p>InitContainer</p>
+            </div>
+        );
+    }
+}
+export default InitContainer;
